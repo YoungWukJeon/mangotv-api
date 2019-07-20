4699ba72094ba966573e4fd4fcb34c2e923045cf
YoungWukJeon <kdh10080@gmail.com>
2019-07-20 17:24:13 +0900

    build.gradle 설정 변경

--- deploy/dev/deploy.sh
+++ deploy/dev/deploy.sh
@@ -4,6 +4,6 @@ echo "SPRING_PROFILE - ${SPRING_PROFILE}"
 
 ## DOCKER PUSH
 ## docker login ~~~
-docker image build -t study/mangotv-api:latest deploy/${SPRING_PROFILE}/Dockerfile \
---build-arg SPRING_PROFILE=${SPRING_PROFILE} && \
+# docker image build -t study/mangotv-api:latest deploy/${SPRING_PROFILE}/Dockerfile -e SPRING_PROFILE=${SPRING_PROFILE}
+docker image build -t study/mangotv-api:latest . --build-arg SPRING_PROFILE=dev
 docker container run -t -p 9000:8080 study/mangotv-api:latest
