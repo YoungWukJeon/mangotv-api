9d1be18994f03e1cbc2917ec6c0d87f0cb68ecb8
PARK SUNG DONG <pasudo123@naver.com>
2019-07-30 23:57:06 +0900

    [feat] add Jpa Auditing

---
+++ src/main/java/com/study/mangotv/config/JapAuditingConfiguration.java
@@ -0,0 +1,16 @@
+package com.study.mangotv.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Created by pasudo123 on 2019-07-30
+ * Blog: https://pasudo123.tistory.com/
+ * Email: oraedoa@gmail.com
+ **/
+@Configuration
+@EnableJpaAuditing
+public class JapAuditingConfiguration {
+
+
+}
