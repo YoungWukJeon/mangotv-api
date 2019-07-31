18ee0ba9817fb3538e8926fa1834549c9c112965
PARK SUNG DONG <pasudo123@naver.com>
2019-07-31 20:43:52 +0900

    [refactor] 불필요 구문 제거 및 익셉션 처리.

--- src/main/java/com/study/mangotv/exception/RestExceptionHandler.java
+++ src/main/java/com/study/mangotv/exception/RestExceptionHandler.java
@@ -1,24 +1,22 @@
 package com.study.mangotv.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-/**
- * Created by pasudo123 on 2019-07-30
- * Blog: https://pasudo123.tistory.com/
- * Email: oraedoa@gmail.com
- **/
 @RestControllerAdvice
 public class RestExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse userNotFoundException(Exception ex){
+    public ResponseEntity<ErrorResponse> userNotFoundException(Exception ex){
 
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(ex.getMessage());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
 
-        return null;
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
-
 }
--- src/main/java/com/study/mangotv/exception/UserNotFoundException.java
+++ src/main/java/com/study/mangotv/exception/UserNotFoundException.java
@@ -1,10 +1,5 @@
 package com.study.mangotv.exception;
 
-/**
- * Created by pasudo123 on 2019-07-30
- * Blog: https://pasudo123.tistory.com/
- * Email: oraedoa@gmail.com
- **/
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message){
