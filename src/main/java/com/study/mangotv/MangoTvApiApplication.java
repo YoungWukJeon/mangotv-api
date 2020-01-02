9cd9a352fa796f2cffa079b100060b43af89d170
YoungWukJeon <kdh10080@gmail.com>
2020-01-02 19:30:21 +0900

    4. redis로 세션 적용 (아직 안됨)

--- src/main/java/com/study/mangotv/MangoTvApiApplication.java
+++ src/main/java/com/study/mangotv/MangoTvApiApplication.java
@@ -3,7 +3,9 @@ package com.study.mangotv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {"com.study.mangotv"})
 public class MangoTvApiApplication {
